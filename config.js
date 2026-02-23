db9240d6e8e5409fc7fcaaf9b2328a3fd1eecde7
riter9873-ux <riter9873@gmail.com>
2026-02-24 02:45:55 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+apiKey: "AIzaSyBkDJvYxri-fSlPVKjZrHfgoubSxRuoBOQ",
+authDomain: "testmode-df610.firebaseapp.com",
+projectId: "testmode-df610",
+appId: "1:753030117752:web:8c1bfe3a57a779a441308d"
+};
